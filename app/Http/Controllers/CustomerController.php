d8fde0da0e90f66b459c78a8f53bca086a510fa1
Md.Mohib Ullah <moin.uoda@gmail.com>
2020-09-02 18:03:57 +0600

    fix checkout page

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+        $customer->first_name = $request->get('first_name');
+        $customer->last_name = $request->get('last_name');
+        $customer->phone = $request->get('phone');
+        $customer->address = $request->get('address');
+        $customer->save();
+        $request->session()->put('customer_id', $customer->id);
+
+        return redirect()->to('/invoice');
+    }
+}
