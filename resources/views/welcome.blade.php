07655294f2342516b12c13ae1a7f6c4c9a8de426
Md.Mohib Ullah <moin.uoda@gmail.com>
2020-09-13 18:25:44 +0600

    css not found issue solved

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ env('APP_NAME') }}</title>
 
-        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+		@if (env('APP_ENV') === 'local')
+			<link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+		@else 
+			<link href="{{ URL::asset('public/css/app.css') }}" rel="stylesheet" type="text/css" >
+		@endif
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
