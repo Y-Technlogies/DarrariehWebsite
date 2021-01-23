31c2dd95d115f95693314ba94ea3ac3020c75185
Md.Mohib Ullah <moin.uoda@gmail.com>
2021-01-24 01:15:42 +0600

    -- fix address field bug

--- resources/lang/en/placeholder.php
+++ resources/lang/en/placeholder.php
@@ -1,4 +1,7 @@
 <?php
  return [
-    'address' => 'Type your address and other notes you\'d like to include, for example size measurement long/chest/sleeve/shoulders'
+    'address' => 'Type your address and other notes you\'d like to include, for example size measurement long/chest/sleeve/shoulders',
+     'first_name' => 'Enter first name',
+     'last_name' => 'Enter last name',
+     'phone' => 'Enter phone number'
  ];
